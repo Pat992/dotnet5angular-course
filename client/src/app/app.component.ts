63f1289d50f741ecc49832d20564c2feccb68124
Patrick Hettich <patrick.hettich@quibiq.ch>
2021-08-04 16:45:27 +0200

    updated users

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'The Dating App';
+  title = 'App';
   users: any;
 
   constructor(private http: HttpClient) { }
